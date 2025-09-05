c1573ee937c55cdaef3cc25b3e3dccfa3943f5e3
Paridhi Mittal <paridhimittal0210@gmail.com>
2025-09-06 01:51:59 +0530

    frontend addedd

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
+import UserContext from './context/UserContext.jsx';
+import CaptainContext from './context/CapatainContext.jsx';
+import SocketProvider from './context/SocketContext.jsx';
+import React from 'react';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <CaptainContext>
+    <UserContext>
+      <SocketProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SocketProvider>
+    </UserContext>
+  </CaptainContext>
+
 )
